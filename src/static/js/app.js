28335a918ba487a7522d9ea1f98f8160c2929bf5
nizurasa <81741396+nizurasa@users.noreply.github.com>
2022-01-02 22:25:18 +0900

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -90,6 +90,7 @@ function AddItemForm({ onNewItem }) {
     };
 
     return (
+        <h1>ToDo App by Imamura Daigo</h1>
         <Form onSubmit={submitNewItem}>
             <InputGroup className="mb-3">
                 <Form.Control
